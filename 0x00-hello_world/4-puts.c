8bbc60c2eeb652f76dfc850d844eaf725560746a
Ikechukwu Kalu <iyke4web@gmail.com>
2022-10-13 13:18:31 -0700

    Add tasks 1-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle".
+ * Return: 0 will be the return value under normal conditions.
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
