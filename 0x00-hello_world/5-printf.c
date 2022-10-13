c7c1cb2e61385a09ded7412685dcfd3fe1628fbd
Ikechukwu Kalu <iyke4web@gmail.com>
2022-10-13 13:39:10 -0700

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece or art,
+ * * Return: 0 will be the return value under normal conditions.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, the outcome is a piece or art\n");
+	return (0);
+}
