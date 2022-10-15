957214fc696739cfe643303b31b70712158df767
Ikechukwu Kalu <iyke4web@gmail.com>
2022-10-14 20:39:55 -0700

    Add task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * followed by a new line
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+	putchar(num);
+	if (num != 57)
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
